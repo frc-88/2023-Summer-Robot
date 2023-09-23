228073c23d27b41fee1284deb40312e08eda48c2
LEELA <Leeladasti@gmail.com>
2023-09-23 15:35:45 -0400

    Created move rollers command and button.

--- src/main/java/frc/robot/subsystems/Arm/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm/Arm.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems.Arm;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants;
+
 public class Arm {
     
+    private final WPI_TalonFX m_Cuberoller = new WPI_TalonFX(Constants.CUBE_ROLLER);
+    private final WPI_TalonFX m_Coneroller = new WPI_TalonFX(Constants.CONE_ROLLER);
+
+   public void MoveRollers() {
+    m_Cuberoller.set(0.5);
+    }
+
+    public CommandBase Moverollers() {
+      return new InstantCommand(() -> {MoveRollers();});
+
+    }
 }
